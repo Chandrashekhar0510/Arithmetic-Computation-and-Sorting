e8aedff0d673d68ace5ec4332e3f5f48097df380
Chandrashekhar0510 <rathodsahul.gmail.com>
2020-06-14 14:23:19 +0530

    UC-1 : Taking the three inputs

--- Arithmetic-Comp-and-sorting.sh
+++ Arithmetic-Comp-and-sorting.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
 echo "welcome to arithmetic computation and sorting problem"
+
+read -p "Enter first number : " a
+read -p "Enter first number : " b
+read -p "Enter first number : " c
+
+echo "Entered number are : " $a $b $c
